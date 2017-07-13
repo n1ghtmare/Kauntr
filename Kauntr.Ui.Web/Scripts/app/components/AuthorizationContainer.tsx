7f0c3d67fef76a857cdede169be5b0c81fcaf8c2
Dimitar Dimitrov <dimitrov2k@gmail.com>
2017-07-13 20:17:54 +0400

    Fixed a bug in the way the AuthorizationContainer was working (with the redirects for authenticated users)

--- Kauntr.Ui.Web/Scripts/app/components/AuthorizationContainer.tsx
+++ Kauntr.Ui.Web/Scripts/app/components/AuthorizationContainer.tsx
@@ -6,8 +6,7 @@ import SharedContextState from "../interfaces/SharedContextState";
 
 export class AuthorizationContainer extends React.Component<SharedContextState, any> {
     componentDidMount() {
-        // BUG - when the user is in a component that requires authorization (and the user is already authorized) - refreshing the page causes him to be redirected to the login
-        if (this.props.currentUserId === null) {
+        if (this.props.currentUserId === null && !this.props.isLoadingData) {
             this.props.router.push("/login?returnUrl=" + this.props.returnUrl);
         }
     }
