8731bc2f4f55f2c91444015ed96f54222d24d938
Dimitar Dimitrov <dimitrov2k@gmail.com>
2017-07-27 23:52:33 +0400

    Initial draft version of the CountdownList layout

--- Kauntr.Ui.Web/Scripts/app/components/Countdown.tsx
+++ Kauntr.Ui.Web/Scripts/app/components/Countdown.tsx
@@ -3,7 +3,11 @@ import * as React from "react";
 export default class Countdown extends React.Component<any, any> {
     render() {
         return (
-            <div>This is the Countdown component</div>
+            <div className="animated fadeIn">
+                <div className="row">
+                    Individual Countdown ...
+                </div>
+            </div>
         );
     }
 }
--- Kauntr.Ui.Web/Scripts/app/components/CountdownList.tsx
+++ Kauntr.Ui.Web/Scripts/app/components/CountdownList.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 import AppState from "../interfaces/AppState";
 
 export default class CountdownList extends React.Component<any, any> {
     render() {
         return (
-            <h1>Countdown List</h1>
+            <div className="animated fadeIn">
+                <div className="row">
+                    <h3>countdowns / trending</h3>
+                    <h4>37</h4>
+                    <div>
+                        countdown list here ...
+                    </div>
+                </div>
+            </div>
         );
     }
 }
