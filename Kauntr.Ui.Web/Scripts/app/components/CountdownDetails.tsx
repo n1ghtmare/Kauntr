f8e0ab592d1456cb3beaead51236c9dbf2938105
Dimitar Dimitrov <dimitrov2k@gmail.com>
2017-09-09 17:38:54 +0400

    Added a prototype for the countdown comments

--- Kauntr.Ui.Web/Scripts/app/components/CountdownDetails.tsx
+++ Kauntr.Ui.Web/Scripts/app/components/CountdownDetails.tsx
@@ -14,6 +14,7 @@ import CountdownState from "../interfaces/CountdownState";
 
 import Countdown from "./Countdown";
 import LoadingIndicator from "./LoadingIndicator";
+import DiamondsSeparator from "./DiamondsSeparator";
 
 export class CountdownDetails extends React.Component<CountdownState, any> {
     componentDidMount() {
@@ -30,6 +31,44 @@ export class CountdownDetails extends React.Component<CountdownState, any> {
             <div className="animated fadeIn">
                 <div className="row">
                     <Countdown {...this.props} />
+                    <DiamondsSeparator />
+
+                    {/* TODO - Remove these prototype comments left for design purposes */}
+                    <div><h4>23 comments</h4></div>
+
+                    <div className="comment">
+                        <div>created 3 days ago</div>
+                        <div>by <a href="#">Haxor</a></div>
+                        <div className="avatar-image-container">
+                            <img width="42" height="42" alt="Avatar Image" src="http://www.gravatar.com/avatar/3c65be11257c594389753a6b5de12569?s=42&d=mm" />
+                        </div>
+                        <div className="comment-text">
+                            My test comment here yo My test comment here yoMy test comment here yoMy test comment here yoMy test comment here yoMy test comment here yoMy test comment here yoMy test comment here yoMy test comment here yoMy test comment here yo
+                        </div>
+                        <div className="comment-score">
+                            <a title="This is awesome" className="vote-up" href="#">&#43;</a>
+                            <span>0</span>
+                            <a title="I don't like it" className="vote-down" href="#">&minus;</a>
+                        </div>
+                    </div>
+
+
+                    <div className="comment">
+                        <div>created 3 days ago</div>
+                        <div>by <a href="#">Haxor</a></div>
+                        <div className="avatar-image-container">
+                            <img width="42" height="42" alt="Avatar Image" src="http://www.gravatar.com/avatar/3c65be11257c594389753a6b5de12569?s=42&d=mm" />
+                        </div>
+                        <div>
+                            My test comment here yo My test comment here yoMy test comment here yoMy test comment here yoMy test comment here yoMy test comment here yoMy test comment here yoMy test comment here yoMy test comment here yoMy test comment here yo
+                        </div>
+                        <div className="comment-score">
+                            <a title="This is awesome" className="vote-up" href="#">&#43;</a>
+                            <span>0</span>
+                            <a title="I don't like it" className="vote-down" href="#">&minus;</a>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         );
