a3234573d45fc8feb769a20628c8ed1c4d6128d4
Dimitar Dimitrov <dimitrov2k@gmail.com>
2017-10-08 22:13:26 +0400

    Added a Production webpack config, and updated .gitignore to include the JS build/dist folder

---
+++ Kauntr.Ui.Web/webpack.config.prod.js
@@ -0,0 +1,39 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+    entry: [
+        "./Scripts/app/boot/App.tsx"
+    ],
+    output: {
+        path: path.join(__dirname, 'Scripts/dist'),
+        filename: 'bundle.js',
+        /*publicPath: '/static/'*/
+    },
+        resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loaders: ["ts-loader"],
+                include: path.join(__dirname, 'Scripts/app')
+            }
+        ]
+    }
+};
