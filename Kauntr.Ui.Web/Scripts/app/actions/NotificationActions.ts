2f8611e735e0666db00f2b6b84c01be5a24f4389
Dimitar Dimitrov <dimitrov2k@gmail.com>
2017-11-06 18:54:29 +0400

    Notifications redux actions/action creators

--- Kauntr.Ui.Web/Scripts/app/actions/NotificationActions.ts
+++ Kauntr.Ui.Web/Scripts/app/actions/NotificationActions.ts
@@ -54,19 +54,53 @@ function loadNotificationsFailure() {
     };
 }
 
-export function dismissNotificationIfNeeded(id: number) {
-    // TODO - dismiss server side
-}
+export function dismissNotificationsAll() {
+    return async (dispatch: Function, getState: Function) => {
+        dispatch({ type: ActionTypes.DISMISS_NOTIFICATIONS_ALL });
 
-function dismissNotification() {
-    return {
-        type: ActionTypes.DISMISS_NOTIFICATION
+        const response: Response = await fetch("notifications/dismissall", { method: "post", headers: { "Content-Type": "application/json" } });
+        if (response.ok) {
+            const json = await response.json();
+            dispatch(dismissNotificationSuccess(json));
+        }
+        else {
+            handleServerError(response, dispatch, loadNotifications);
+        }
     };
 }
 
-function dismissNotificationSuccess() {
+function dismissNotificationsAllSuccess() {
     return {
         type: ActionTypes.DISMISS_NOTIFICATION_SUCCESS
+    };
+}
+
+function dismissNotificationsAllFailure() {
+    return {
+        type: ActionTypes.DISMISS_NOTIFICATION_FAILURE
+    };
+}
+
+export function dismissNotification(id: number) {
+    return async (dispatch: Function, getState: Function) => {
+        dispatch({ type: ActionTypes.DISMISS_NOTIFICATION });
+
+        const data: string = JSON.stringify({ Id: id });
+        const response: Response = await fetch("notifications/id", { method: "post", body: data, headers: { "Content-Type": "application/json" } });
+        if (response.ok) {
+            const json = await response.json();
+            dispatch(dismissNotificationSuccess(json));
+        }
+        else {
+            handleServerError(response, dispatch, loadNotifications);
+        }
+    };
+}
+
+function dismissNotificationSuccess(json: any) {
+    return {
+        type: ActionTypes.DISMISS_NOTIFICATION_SUCCESS,
+        json
     };
 }
 
